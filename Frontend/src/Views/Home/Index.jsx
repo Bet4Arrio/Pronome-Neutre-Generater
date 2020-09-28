7fa067487df3e2028fdc662bbfc8f7fe140186ed
Everson Vansoski <eversonvansoski@gmail.com>
2020-09-28 16:37:19 -0300

    testes de estilo

--- Frontend/src/Views/Home/Index.jsx
+++ Frontend/src/Views/Home/Index.jsx
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 export default class Index extends Component {
   render() {
-    return <div className="titulo">Pronome Neutre Generater</div>;
+    return (
+      <>
+        <h1 className="titulo">Pronome Neutre Generater</h1>
+        <input type="text" className="busca" />
+      </>
+    );
   }
 }
